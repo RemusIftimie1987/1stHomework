c419660f389a47dba10eaae04eae3f5e0aa629e2
Remus Iftimie <RIftimie@spartaglobal.com>
2019-10-28 18:32:56 +0000

    My 1st Homework

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DriverlessCarsClasses
+{
+    class Vehicle
+    {
+        public string name;
+        public int year;
+        public double price;
+    }
+    class DriverlessCar
+    {
+        public int automation_level;
+    }
+    class CompleteAutomation
+    {
+        public bool havePedals;
+    }
+    class HighAutomation
+    {
+        public int numberOfSensors;
+    }
+    class ConditionalAutomation
+    {
+        private bool require_human_input;
+    }
+    class ManualCar
+    {
+        public string transmission_type;
+    }
+    class NoAutomation
+    {
+        public int type;
+    }
+    class DriverAssistance
+    {
+        public string assist_functions;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
